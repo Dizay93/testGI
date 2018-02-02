ad656a8addc404e3918f06984c1a3d7a2e08521a
Yazid Elbadri <yelbadri@sqli.com>
2018-02-02 10:46:25 +0000

    dizay commit

--- App.java
+++ App.java
@@ -4,7 +4,7 @@ public static void main (String [] args){
 
 
 	System.out.println("La vie est belle a SQLi ");
-
+	int i =0;
 
 
 	
@@ -14,5 +14,7 @@ public void helloWorld(){
 	System.out.println("Hello World");	
 
 }
+
+
 
 }
